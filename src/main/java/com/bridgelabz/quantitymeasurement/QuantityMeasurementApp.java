d24c916f9b7112e64d03baa9339083bc72a6a90c
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-06-19 11:41:56 +0530

    UC2: Feet and Inches measurement equality

--- src/main/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementApp.java
@@ -11,9 +11,47 @@ public class QuantityMeasurementApp {
         @Override
         public boolean equals(Object obj) {
             if (this == obj) return true;
-            if (obj == null || getClass() != obj.getClass()) return false;
-            Feet feet = (Feet) obj;
-            return Double.compare(feet.value, value) == 0;
+            if (obj == null) return false;
+            if (getClass() == obj.getClass()) {
+                Feet feet = (Feet) obj;
+                return Double.compare(feet.value, value) == 0;
+            }
+            if (obj instanceof Inches) {
+                Inches inches = (Inches) obj;
+                return Double.compare(inches.getValue(), value * 12.0) == 0;
+            }
+            return false;
+        }
+
+        public double getValue() {
+            return value;
+        }
+    }
+
+    public static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null) return false;
+            if (getClass() == obj.getClass()) {
+                Inches inches = (Inches) obj;
+                return Double.compare(inches.value, value) == 0;
+            }
+            if (obj instanceof Feet) {
+                Feet feet = (Feet) obj;
+                return Double.compare(feet.getValue() * 12.0, value) == 0;
+            }
+            return false;
+        }
+
+        public double getValue() {
+            return value;
         }
     }
 
