b9b129b58c87925695ecadacfbc582636368bca9
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-06-19 12:00:52 +0530

    UC9: Weight Measurement Equality, Conversion, and Addition

---
+++ src/main/java/com/bridgelabz/quantitymeasurement/WeightUnit.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.quantitymeasurement;
+
+public enum WeightUnit {
+    KILOGRAM(1.0),
+    GRAM(0.001),
+    POUND(0.453592);
+
+    private final double baseUnitConversionFactor;
+
+    WeightUnit(double baseUnitConversionFactor) {
+        this.baseUnitConversionFactor = baseUnitConversionFactor;
+    }
+
+    public double convertToBaseUnit(double value) {
+        return value * this.baseUnitConversionFactor;
+    }
+
+    public double convertFromBaseUnit(double baseValue) {
+        return baseValue / this.baseUnitConversionFactor;
+    }
+}
